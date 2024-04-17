342a83b7b502b48e9e0d24367c3329146dd08b75
judzink <eugencomnosan@hotmail.com>
2024-04-17 18:02:30 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,62 @@
 function App() {
-	return <div>Simple Test</div>
+	return (
+		<div style={{ display: "flex", gap: "20px", height: "100%" }}>
+			{/* Section 1 */}
+			<div
+				style={{
+					background: "white",
+					width: "320px",
+					borderRadius: "8px",
+					padding: "12px",
+				}}
+			>
+				Active Session
+			</div>
+
+			<div
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					width: "100%",
+					gap: "20px",
+				}}
+			>
+				{/* Navbar */}
+				<div
+					style={{ background: "white", borderRadius: "8px", padding: "12px" }}
+				>
+					Header
+				</div>
+				{/* content wrapper  */}
+				<div style={{ display: "flex", gap: "20px", height: "100%" }}>
+					{/* content */}
+					<div
+						style={{
+							width: "100%",
+							background: "white",
+							borderRadius: "8px",
+							padding: "12px",
+							height: "100%",
+						}}
+					>
+						Management
+					</div>
+					{/* jobs */}
+					<div
+						style={{
+							width: "320px",
+							background: "white",
+							borderRadius: "8px",
+							padding: "12px",
+							height: "100%",
+						}}
+					>
+						Schedules Jobs
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+	return <div style={{ width: "320px", height: "100%" }}>Navbar</div>
+}
+
+export default Navbar
