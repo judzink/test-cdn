9d64fa2fc042e6a8928eea45fac04d6b201fb30f
judzink <46721806+judzink@users.noreply.github.com>
2024-04-17 17:30:00 +0200

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	// const [visibleRating, setVisibleRating] = useState(false)
+	// const [visibleComment, setVisibleComment] = useState(false)
+	// const [visibleReward, setVisibleReward] = useState(false)
+
+	return <div>Simple Test</div>
+}
+
+export default App
